32510ef346c545dfe3c114b37f8d913aa10aac88
Christoph Schrade <schrade.christoph@gmail.com>
2022-10-11 13:55:11 +0200

    Print message if old data is loaded

--- algo/operator.py
+++ algo/operator.py
@@ -50,8 +50,10 @@ class Operator(util.OperatorBase):
             df.index = pd.to_datetime(df.index)
             data_series = pd.Series(data=df['power_values'], index=df.index)
             data_series = df[~df.index.duplicated(keep='first')]
-        for i in range(len(data_series.index)):
-            self.anomaly_detector.data.append([self.todatetime(data_series.index[i]), float(data_series.iloc[i])])
+            for i in range(len(data_series.index)):
+                self.anomaly_detector.data.append([self.todatetime(data_series.index[i]), float(data_series.iloc[i])])
+            print('Old data loaded.')
+        
 
         if os.path.exists(self.anomaly_detector_device_type_path):
             with open(self.anomaly_detector_device_type_path, 'rb') as f:
