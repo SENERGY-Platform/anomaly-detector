651f079d62cf81a4130b0b2822805ee24b5e85f4
Christoph Schrade <schrade.christoph@gmail.com>
2023-12-12 15:12:20 +0100

    fix run method

--- algo/operator.py
+++ algo/operator.py
@@ -38,17 +38,19 @@ class Operator(util.OperatorBase):
 
         self.device_name = device_name
 
+        self.active = []
+
         if check_data_anomalies:
             self.Curve_Explorer = curve_anomaly.Curve_Explorer(data_path)
+            self.active.append(self.Curve_Explorer)
         
         if check_data_extreme_outlier:
             self.Point_Explorer = point_outlier.Point_Explorer()
+            self.active.append(self.Point_Explorer)
+        
 
     def run(self, data, selector='energy_func'):
-        for operator in [
-            {"name": "Point Outlier Detector", "detector": self.Point_Explorer},
-            {"name": "Anomaly Detector", "detector": self.Curve_Explorer},
-        ]:
+        for operator in self.active:
             sample_is_anomalous, message = operator.run(data)
 
             if sample_is_anomalous:
