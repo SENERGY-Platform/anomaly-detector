d936414630b9cd096894ca8c06fb8a20182de825
Christoph Schrade <schrade.christoph@gmail.com>
2024-01-15 11:39:57 +0100

    implement data persistence for consumption_explorer

--- algo/consumption_anomaly/consumption_explorer.py
+++ algo/consumption_anomaly/consumption_explorer.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from algo import utils
 from . import consumption_utils
+import pickle
+import os
 
 __all__ = ("Consumption_Explorer",)
 
 class Consumption_Explorer:
-    def __init__(self):
-
+    def __init__(self, data_path):
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+        
         self.first_data_time = None
         self.history_time_span = pd.Timedelta(14,'days')
         #TODO: Think about whether it's reasonable to make this a config.
@@ -19,7 +23,44 @@ class Consumption_Explorer:
 
         self.linear_model = None
 
-        #TODO: Implement data persistence.
+        self.filename_dict = {"first_data_time": f'{data_path}/first_data_time.pickle', "data_history": f'{data_path}/data_history.pickle', 
+                              "last_estimate_time": f'{data_path}/last_estimate_time.pickle', "linear_model": f'{data_path}/linear_model.pickle'}
+        
+        self.load_data()
+    
+    def save_data(self):
+        first_data_time_path = self.filename_dict["first_data_time"]
+        data_history_path = self.filename_dict["data_history"]
+        last_estimate_time_path = self.filename_dict["last_estimate_time"]
+        linear_model_path = self.filename_dict["linear_model"]
+
+        with open(first_data_time_path, 'wb') as f:
+            pickle.dump(self.first_data_time, f)
+        with open(data_history_path, 'wb') as f:
+            pickle.dump(self.data_history, f)
+        with open(last_estimate_time_path, 'wb') as f:
+            pickle.dump(self.last_estimate_time, f)
+        with open(linear_model_path, 'wb') as f:
+            pickle.dump(self.linear_model, f)
+
+    def load_data(self):
+        first_data_time_path = self.filename_dict["first_data_time"]
+        data_history_path = self.filename_dict["data_history"]
+        last_estimate_time_path = self.filename_dict["last_estimate_time"]
+        linear_model_path = self.filename_dict["linear_model"]
+
+        if os.path.exists(first_data_time_path):
+            with open(first_data_time_path, 'rb') as f:
+                self.first_data_time = pickle.load(f)
+        if os.path.exists(data_history_path):
+            with open(data_history_path, 'rb') as f:
+                self.data_history = pickle.load(f)
+        if os.path.exists(last_estimate_time_path):
+            with open(last_estimate_time_path, 'rb') as f:
+                self.last_estimate_time = pickle.load(f)
+        if os.path.exists(linear_model_path):
+            with open(linear_model_path, 'rb') as f:
+                self.linear_model = pickle.load(f)
 
     def run(self, data):
         timestamp = utils.todatetime(data['energy_time']).tz_localize(None)
@@ -36,6 +77,8 @@ class Consumption_Explorer:
         else:
             if timestamp - self.last_estimate_time >= self.step_until_next_estimate:
                 self.linear_model, self.last_estimate_time = consumption_utils.update_linear_model(self.data_history)
+
+        self.save_data()
 
         if timestamp - self.first_data_time >= pd.Timedelta(2,'days'):
             anomaly_occured = consumption_utils.check_outlier(self.data_history, self.linear_model)
