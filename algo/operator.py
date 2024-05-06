a72ecfcd6e199ad42fb7f64caaffc46c221ea7db
Hannes Hansen <mail@hannesh.de>
2024-05-06 12:28:19 +0200

    updated init handling

--- algo/operator.py
+++ algo/operator.py
@@ -113,24 +113,12 @@ class Operator(OperatorBase):
         timestamp = todatetime(data['time'])
         value = float(data['value'])
         util.logger.debug(f'{LOG_PREFIX}: Device: {device_id} Input time: {str(data["time"])} Value: {str(data["value"])}')
-
-        init_value = {
-            "type": False,
-            "sub_type": "",
-            "value": "",
-            "threshold": 0,
-            "mean": 0
-        }
-        if self.init_phase_handler.operator_is_in_init_phase(timestamp):
-            return self.init_phase_handler.generate_init_msg(timestamp, init_value)
         
-        if self.init_phase_handler.init_phase_needs_to_be_reset():
-            return self.init_phase_handler.reset_init_phase(init_value)
- 
+        operator_is_init = self.init_phase_handler.operator_is_in_init_phase(timestamp)
         device_detector = self.get_device_detectors(input_id)
         anomalies_found = None
         timestamp_without_tz = timestamp.tz_localize(None)
-        if self.input_is_real_time(timestamp):
+        if self.input_is_real_time(timestamp) and not operator_is_init:
             device_detector.start_freq_loop()
             util.logger.debug(f"{LOG_PREFIX}: Check input for anomalies")
             anomalies_found = device_detector.check_input(value, timestamp_without_tz)
@@ -139,6 +127,20 @@ class Operator(OperatorBase):
 
         util.logger.debug(f"{LOG_PREFIX}: Register new input at detectors")
         device_detector.update(value, timestamp_without_tz, self.input_is_real_time(timestamp))
+        
+
+        init_value = {
+            "type": False,
+            "sub_type": "",
+            "value": "",
+            "threshold": 0,
+            "mean": 0
+        }
+        if operator_is_init:
+            return self.init_phase_handler.generate_init_msg(timestamp, init_value)
+
+        if self.init_phase_handler.init_phase_needs_to_be_reset():
+            return self.init_phase_handler.reset_init_phase(init_value)
         
         if anomalies_found:
             return anomalies_found
--- algo/utils.py
+++ algo/utils.py
@@ -18,17 +18,15 @@ class StdPointOutlierDetector():
             os.makedirs(data_path)
         
         self.filename_dict = {"current_stddev": f'{data_path}/current_stddev_point.pickle', "current_mean": f'{data_path}/current_mean_point.pickle', 
-                              "num_datepoints": f'{data_path}/num_datepoints_point.pickle', "first_data_time": f'{data_path}/first_data_time_point.pickle'}
+                              "num_datepoints": f'{data_path}/num_datepoints_point.pickle'}
         
         self.current_stddev = 0
         self.current_mean = 0
         self.num_datepoints = 0
-        self.first_data_time = None
 
         (self.current_stddev, 
         self.current_mean, 
-        self.num_datepoints, 
-        self.first_data_time) = self.load_data(self.current_stddev, 
+        self.num_datepoints) = self.load_data(self.current_stddev, 
                                               self.current_mean, 
                                               self.num_datepoints, 
                                               self.first_data_time)
@@ -54,18 +52,15 @@ class StdPointOutlierDetector():
             pickle.dump(self.current_mean, f)
         with open(num_datepoints_path, 'wb') as f:
             pickle.dump(self.num_datepoints, f)
-        with open(first_data_time_path, 'wb') as f:
-            pickle.dump(self.first_data_time, f)
 
     def stop(self):
         util.logger.info("Stop Std Outlier Detector")
         self.save()
 
-    def load_data(self, current_stddev, current_mean, num_datepoints, first_data_time):
+    def load_data(self, current_stddev, current_mean, num_datepoints):
         current_stddev_path = self.filename_dict["current_stddev"]
         current_mean_path = self.filename_dict["current_mean"]
         num_datepoints_path = self.filename_dict["num_datepoints"]
-        first_data_time_path = self.filename_dict["first_data_time"]
         
         if os.path.exists(current_stddev_path):
             with open(current_stddev_path, 'rb') as f:
@@ -76,11 +71,8 @@ class StdPointOutlierDetector():
         if os.path.exists(num_datepoints_path):
             with open(num_datepoints_path, 'rb') as f:
                 num_datepoints = pickle.load(f)
-        if os.path.exists(first_data_time_path):
-            with open(first_data_time_path, 'rb') as f:
-                first_data_time = pickle.load(f)
-        
-        return current_stddev, current_mean, num_datepoints, first_data_time
+ 
+        return current_stddev, current_mean, num_datepoints
 
     def point_is_anomalous_high(self, point):
         if self.num_datepoints < 2:
