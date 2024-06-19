da79a42fe03bbc3ea142114bec4b02f6d4276064
Hannes Hansen <mail@hannesh.de>
2024-06-19 16:45:16 +0200

    outsourced training request and loading

--- algo/curve_anomaly/cont_det/online_detector.py
+++ algo/curve_anomaly/cont_det/online_detector.py
@@ -4,11 +4,7 @@ import torch
 import json
 import pandas as pd
 from . import cont_device
-import requests 
-import mlflow 
-from operator_lib.util.persistence import save, load
-
-JOB_ID_FILENAME = "training_job_id.pickle"
+from operator_lib.util.ml import Trainer
 
 class OnlineTrainContCurveDetector(ContCurveDetector):
     # Used for outsourced training of the model 
@@ -27,18 +23,25 @@ class OnlineTrainContCurveDetector(ContCurveDetector):
         retrain
     ):
         super().__init__(data_path, init_median, first_data_time)
-        self.ml_trainer_url = ml_trainer_url
         self.device_id = device_id
-        self.mlflow_url = mlflow_url
         self.model = None
-        self.job_id = load(data_path, JOB_ID_FILENAME)
         self.operator_id = operator_id
         self.pipeline_id = pipeline_id
         self.anomalies = []
-        self.train_level = train_level
-        self.train_interval = train_interval
-        self.retrain = retrain 
-        self.training_is_running = False
+
+        self.trainer = Trainer(
+            util.logger,
+            data_path,
+            ml_trainer_url,
+            'mlfit',
+            self.model,
+            self.last_training_time,
+            train_interval,
+            train_level,
+            retrain,
+            mlflow_url,
+            1
+        )
 
     def check(self, value, timestamp):
         if self.first_data_time == None:
@@ -50,11 +53,8 @@ class OnlineTrainContCurveDetector(ContCurveDetector):
         if self.data_list[-1][0] - self.data_list[0][0] >= pd.Timedelta(10, "h"): # Only keep data, which is at most 10 hours old.
             del self.data_list[0]
 
-        if self.training_shall_start(timestamp):
+        if self.trainer.training_shall_start(timestamp):
             self.start_training(timestamp)
-
-        if self.model_shall_be_downloaded():
-            self.load_model()
         
         if not self.model:
             util.logger.debug("Model is not available. Skip inference")
@@ -79,23 +79,6 @@ class OnlineTrainContCurveDetector(ContCurveDetector):
         
         return False, ''
             
-    def training_shall_start(self, timestamp):
-        # Training shall start when there is enough initial data or when retraining is enabled
-        util.logger.debug(f"Current Time: {timestamp} - Last Train Time: {self.last_training_time} < {self.train_interval}{self.train_level}")
-        if timestamp - self.last_training_time < pd.Timedelta(self.train_interval, self.train_level):
-            util.logger.debug("Wait with training until enough data is collected")
-            return False 
-
-        if not self.job_id:
-            util.logger.debug("No existing JobID -> Start first training")
-            return True 
-
-        if self.retrain:
-            util.logger.debug("Retrain Period over. Start new training.")
-            return True
-
-        return False
-
     def start_training(self, timestamp):
         topic_name, path_to_time, path_to_value = self._get_input_topic()
         job_request = {
@@ -136,49 +119,15 @@ class OnlineTrainContCurveDetector(ContCurveDetector):
                 "cpu_worker_limit": 2
             }
         }
-        util.logger.debug(f"Start online training")
-        res = requests.post(self.ml_trainer_url + "/mlfit", json=job_request)
-        util.logger.debug(f"ML Trainer Response: {res.text}")
-        if res.status_code != 200:
-            util.logger.error(f"Cant start training job {res.text}")
-            return
-        self.job_id = res.json()['task_id']
-        util.logger.debug(f"Created Training Job with ID: {self.job_id}")
+        self.trainer.start_training(job_request)
         self.last_training_time = timestamp
-        save(self.data_path, JOB_ID_FILENAME, self.job_id)
-        self.training_is_running = True
-
-    def is_job_ready(self):
-        res = requests.get(self.ml_trainer_url + "/job/"+self.job_id)
-        res_data = res.json()
-        job_status = res_data['success'] 
-        util.logger.debug(f"Training Job Status: {job_status}")
-        if job_status == 'error':
-            raise Exception(res_data['response'])
-
-        return job_status == 'done'
     
-    def model_shall_be_downloaded(self): 
-        if not self.training_is_running:
-            util.logger.debug("No need to download model. No training running.")
-            return False 
-
-        if self.job_id and self.is_job_ready():
-            util.logger.debug("Training Job is ready -> model can be downloaded")
-            return True
-
-        util.logger.debug("Training Job is not ready yet")
-        return False
 
     def load_model(self):
-        mlflow.set_tracking_uri(self.mlflow_url)
-        model_uri = f"models:/{self.job_id}@production"
-        util.logger.debug(f"Try to download model {self.job_id}")
-        self.model = mlflow.pyfunc.load_model(model_uri)
-        util.logger.debug(f"Downloading model {self.job_id} was succesfull")
+        # TODO christoph doch lieber model abspeichern beim stoppen und laden 
+        # job nur nehmen wenn noch kein lokales model da ist???
         unwrapped_model = self.model.unwrap_python_model()
         unwrapped_model.set_all_reconstruction_errors(self.reconstruction_errors)
-        self.training_is_running = False
         
     def test(self, data_list, model):
         util.logger.debug("Run inference")
@@ -211,3 +160,4 @@ class OnlineTrainContCurveDetector(ContCurveDetector):
         if self.model:
             self.reconstruction_errors = self.model.unwrap_python_model().get_all_reconstruction_errors()
         super().stop()
+        self.trainer.stop()
