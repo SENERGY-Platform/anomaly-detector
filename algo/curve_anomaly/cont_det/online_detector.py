4a55503d953b306f110d5ca5d6f1ba95d12155a6
Hannes Hansen <mail@hannesh.de>
2024-08-13 12:53:19 +0200

    added user id to training config

--- algo/curve_anomaly/cont_det/online_detector.py
+++ algo/curve_anomaly/cont_det/online_detector.py
@@ -118,6 +118,7 @@ class OnlineTrainContCurveDetector(ContCurveDetector):
             },
             "toolbox_version": "v2.2.76",
             "ray_image": "ghcr.io/senergy-platform/ray:v0.0.9",
+            "user_id": "", # TODO userid is in environment variables, must be set in operator-lib 
             "ray_version": "2.24.0", # must be the same as in the image
             "cluster": {
                 "number_workers": 1,
